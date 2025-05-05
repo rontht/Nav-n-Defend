3432ab517adde24d99c6ad7b186849b6d322aae6
VividStrike <nate.campbell@hotmail.com>
2025-05-05 15:00:03 +1000

    added camera access

---
+++ Assets/Scenes/Scripts/CameraAccess.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    WebCamTexture webcam;
+    public RawImage img;
+    // Start is called before the first frame update
+    void Start()
+    {
+        webcam = new WebCamTexture();
+        img.texture = webcam;
+        webcam.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
