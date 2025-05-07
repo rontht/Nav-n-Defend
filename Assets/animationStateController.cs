ef7ecd90d181e837a29f0060316257d44e2c2cd7
VividStrike <nate.campbell@hotmail.com>
2025-05-07 19:09:37 +1000

    added slime sprites, movement etc

---
+++ Assets/animationStateController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class animationStateController : MonoBehaviour
+{
+    Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        //Debug.Log(animator);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("w"))
+        {
+            animator.SetBool("isWalking", true);
+        }    
+    }
+}
