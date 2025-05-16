046afccb67c9860d147b5aeb3327b479a4a35561
Thwin Htoo Thant <148700124+rontht@users.noreply.github.com>
2025-05-17 01:45:31 +1000

    added some function into settings for testing. sound pack added.

---
+++ Assets/Game/Scripts/UI/SettingManager.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class SettingManager : MonoBehaviour
+{
+    public void ResetButtonFunction()
+    {
+        if (PlayerStats.Instance != null)
+            PlayerStats.Instance.ResetStats();
+        else
+            Debug.Log("Player Stats Instance not found.");
+    }
+
+    public void LevelButtonFunction()
+    {
+        if (PlayerStats.Instance != null)
+            PlayerStats.Instance.GainExperience(10);
+        else
+            Debug.Log("Player Stats Instance not found.");
+    }
+
+    public void CashButtonFunction()
+    {
+        if (PlayerStats.Instance != null)
+            PlayerStats.Instance.AddCoins(10);
+        else
+            Debug.Log("Player Stats Instance not found.");
+    }
+}
